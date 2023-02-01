d6fe678375ea4b23730df2edba44b46dcd3976f7
Ahsan Habib <ahsan.just.cse@gmail.com>
2023-02-01 22:10:53 +0600

    11th commit

---
+++ JS/class.js
@@ -0,0 +1,20 @@
+function show()
+{
+    class Car{
+        constructor(name, year){
+            this.name = name;
+            this.year = year;
+        }
+
+        age(){
+            let date = new Date();
+            return date.getFullYear() - this.year;
+        }
+    }
+
+    let car1 = new Car('Toyota', 2014);
+    let car2 = new Car("Audi", 2019);
+    document.getElementById('demo').innerHTML = 'Age of ' + car1.name + ' is ' + car1.age() + '<br>'+
+    'Age of ' + car2.name + ' is ' + car2.age();
+
+}
